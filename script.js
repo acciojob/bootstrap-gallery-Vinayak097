cffc15678c5bd3586ebf6cb94450fac1864886cb
Vinayak097 <vinayak20injamure@gmail.com>
2026-08-14 10:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const gallaryImages = document.querySelectorAll('.card-img-top');
+
+const carouselElement =
+    document.getElementById("galleryCarousel");
+
+gallaryImages.forEach((e, index) => {
+
+    e.addEventListener('click', () => {
+
+        const carousel =
+            bootstrap.Carousel.getOrCreateInstance(
+                carouselElement
+            );
+
+        carousel.to(index);
+
+    });
+
+});
